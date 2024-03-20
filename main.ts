5b28c2718a0a7874bfd03fb758bc6b0a4875a6ca
HuangYiming0608 <133227329+HuangYiming0608@users.noreply.github.com>
2024-03-20 16:50:55 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,30 @@ input.onButtonPressed(Button.B, function () {
     music.setVolume(255)
     record.playAudio(record.BlockingState.Blocking)
 })
+basic.forever(function () {
+    if (input.soundLevel() > 255) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() > 127) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
